ac298c14b5f6a8bd8ae9938077a92bea9de00e79
Mitchell Elliott <melliott18@github.com>
2020-07-20 19:18:18 -0700

    Split players table into all and active tables

--- players-update.py
+++ players-update.py
@@ -7,104 +7,167 @@ __status__ = "Development"
 	stats API and puts them in the players database.
 """
 
-import mysql.connector
+import jhp
+import os
 import requests
 import time
 
-BASE = "http://statsapi.web.nhl.com/api/v1"
-teams = requests.get("{}/teams".format(BASE)).json()
+def update_all_players():
+    pdb = jhp.db_connect("playersDB")
+    pcursor = pdb.cursor()
+    tdb = jhp.db_connect("teamsDB")
+    tcursor = tdb.cursor()
+    jhp.db_drop_table("playersDB", "all_players")
+    jhp.db_drop_table("playersDB", "active_players")
+    sql ='''CREATE TABLE all_players(
+        player_id INT PRIMARY KEY,
+        player_name CHAR(25),
+        team_id TINYINT(1),
+        team_name CHAR(25),
+        player_type CHAR(25),
+        goals SMALLINT,
+        assists SMALLINT,
+        wins SMALLINT,
+        shutouts TINYINT(1),
+        points SMALLINT,
+        status_id TINYINT(1)
+    )'''
+    jhp.db_create_table("playersDB", sql)
+    sql ='''CREATE TABLE active_players(
+        player_id INT PRIMARY KEY,
+        player_name CHAR(25),
+        team_id TINYINT(1),
+        team_name CHAR(25),
+        player_type CHAR(25),
+        goals SMALLINT,
+        assists SMALLINT,
+        wins SMALLINT,
+        shutouts TINYINT(1),
+        points SMALLINT,
+        status_id TINYINT(1)
+    )'''
+    jhp.db_create_table("playersDB", sql)
+    tcursor.execute("SELECT * FROM teams")
+    teams = tcursor.fetchall()
 
-def db_create():
-	db = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		password="Mitch1669224",
-	)
-	cursor = db.cursor()
-	cursor.execute("CREATE DATABASE IF NOT EXISTS playersDB")
-	db.commit()
-	return db
+    BASE = "http://statsapi.web.nhl.com/api/v1"
 
-def db_connect():
-	db = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		password="Mitch1669224",
-		database='playersDB'
-	)
-	return db
+    for team in teams:
+        team_id = team[0]
+        team_name = team[1]
+        if team[2] == 5:
+            status_id = 1
+        else:
+            status_id = 0
 
-def db_drop_table():
-	db = db_connect()
-	cursor = db.cursor()
-	cursor.execute("DROP TABLE IF EXISTS players")
-	db.commit()
-	return cursor
+        roster = requests.get("{}/teams/{}/roster".format(BASE, team_id)).json()
 
-def db_create_table():
-	db = db_connect()
-	cursor = db.cursor()
-	sql ='''CREATE TABLE players(
-	   player_id INT PRIMARY KEY,
-	   player_name CHAR(25),
-	   team_id TINYINT(1),
-	   team_name CHAR(25),
-	   player_type CHAR(25),
-	   goals SMALLINT,
-	   assists SMALLINT,
-	   wins SMALLINT,
-	   shutouts TINYINT(1),
-	   points SMALLINT
-	)'''
-	cursor.execute(sql)
-	db.commit()
+        if "roster" in roster:
+            for player in roster['roster']:
+                player_id = player['person']['id']
+                player_name = player['person']['fullName']
 
-start = time.time()
+                stats = requests.get("{}/people/{}/stats?stats=statsSingleSeasonPlayoffs&season=20182019".format(BASE, player_id)).json()
 
-db_create()
-count = 1
+                if stats['stats'][0]['splits']:
+                    if "assists" in stats['stats'][0]['splits'][0]['stat']:
+                        player_type = "Skater"
+                        goals = stats['stats'][0]['splits'][0]['stat']['goals']
+                        assists = stats['stats'][0]['splits'][0]['stat']['assists']
+                        wins = None
+                        shutouts = None
+                        points = stats['stats'][0]['splits'][0]['stat']['points']
+                    elif "wins" in stats['stats'][0]['splits'][0]['stat']:
+                        player_type = "Goalie"
+                        goals = None
+                        assists = None
+                        wins = stats['stats'][0]['splits'][0]['stat']['wins']
+                        shutouts = stats['stats'][0]['splits'][0]['stat']['shutouts']
+                        points = (wins * 2) + shutouts
 
-while(True):	
-	db_drop_table()
-	db_create_table()
-	db = db_connect()
-	cursor = db.cursor()
+                    sql = "INSERT INTO all_players (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    val = (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id)
+                    pcursor.execute(sql, val)
 
-	current = time.time()
+                    if status_id:
+                        sql = "INSERT INTO active_players (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        val = (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id)
+                        pcursor.execute(sql, val)
+                    pdb.commit()
+    pdb.close()
+    tdb.close()
+    print("All players updated")
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
 
-	print(current - start)
-	count += 1
+def update_active_players():
+    jhp.db_drop_table("playersDB", "active_players")
+    sql ='''CREATE TABLE active_players(
+        player_id INT PRIMARY KEY,
+        player_name CHAR(25),
+        team_id TINYINT(1),
+        team_name CHAR(25),
+        player_type CHAR(25),
+        goals SMALLINT,
+        assists SMALLINT,
+        wins SMALLINT,
+        shutouts TINYINT(1),
+        points SMALLINT,
+        status_id TINYINT(1)
+    )'''
+    jhp.db_create_table("playersDB", sql)
+    pdb = jhp.db_connect("playersDB")
+    pcursor = pdb.cursor()
+    tdb = jhp.db_connect("teamsDB")
+    tcursor = tdb.cursor()
+    tcursor.execute("SELECT * FROM teams")
+    teams = tcursor.fetchall()
 
-	for team in teams['teams']:
-		team_id = team['id']
-		team_name = team['name']
-		roster = requests.get("{}/teams/{}/roster".format(BASE, team_id)).json()
+    BASE = "http://statsapi.web.nhl.com/api/v1"
 
-		if "roster" in roster:
-			for player in roster['roster']:
-				player_id = player['person']['id']
-				player_name = player['person']['fullName']
-				stats = requests.get("{}/people/{}/stats?stats=statsSingleSeasonPlayoffs&season=20182019".format(BASE, player_id)).json()
+    for team in teams:
+        team_id = team[0]
+        team_name = team[1]
+        if team[2] == 5:
+            status_id = 1
+            roster = requests.get("{}/teams/{}/roster".format(BASE, team_id)).json()
 
-				if stats['stats'][0]['splits']:
-					if "assists" in stats['stats'][0]['splits'][0]['stat']:
-						player_type = "Skater"
-						goals = stats['stats'][0]['splits'][0]['stat']['goals']
-						assists = stats['stats'][0]['splits'][0]['stat']['assists']
-						wins = None
-						shutouts = None
-						points = stats['stats'][0]['splits'][0]['stat']['points']
-					elif "wins" in stats['stats'][0]['splits'][0]['stat']:
-						player_type = "Goalie"
-						goals = None
-						assists = None
-						wins = stats['stats'][0]['splits'][0]['stat']['wins']
-						shutouts = stats['stats'][0]['splits'][0]['stat']['shutouts']
-						points = (wins * 2) + shutouts
-					sql = "INSERT INTO players (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-					val = (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points)
-					cursor.execute(sql, val)
-					db.commit()
-	#time.sleep(600)
-				
-db.close()
+            if "roster" in roster:
+                for player in roster['roster']:
+                    player_id = player['person']['id']
+                    player_name = player['person']['fullName']
+                    stats = requests.get("{}/people/{}/stats?stats=statsSingleSeasonPlayoffs&season=20182019".format(BASE, player_id)).json()
+
+                    if stats['stats'][0]['splits']:
+                        if "assists" in stats['stats'][0]['splits'][0]['stat']:
+                            player_type = "Skater"
+                            goals = stats['stats'][0]['splits'][0]['stat']['goals']
+                            assists = stats['stats'][0]['splits'][0]['stat']['assists']
+                            wins = None
+                            shutouts = None
+                            points = stats['stats'][0]['splits'][0]['stat']['points']
+                        elif "wins" in stats['stats'][0]['splits'][0]['stat']:
+                            player_type = "Goalie"
+                            goals = None
+                            assists = None
+                            wins = stats['stats'][0]['splits'][0]['stat']['wins']
+                            shutouts = stats['stats'][0]['splits'][0]['stat']['shutouts']
+                            points = (wins * 2) + shutouts
+
+                        sql = "INSERT INTO active_players (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        val = (player_id, player_name, team_id, team_name, player_type, goals, assists, wins, shutouts, points, status_id)
+                        pcursor.execute(sql, val)
+                        pdb.commit()
+
+    pdb.close()
+    tdb.close()
+    print("All active players updated")
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
+
+#while(True):	
+update_all_players()
+#update_active_players()
+    #time.sleep(600)
