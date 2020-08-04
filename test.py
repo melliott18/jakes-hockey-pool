61ff05479ffb724d30f7fe66f23eccc5cf9ee006
Mitchell Elliott <melliott18@github.com>
2020-08-03 20:25:45 -0700

    Added test.py

---
+++ test.py
@@ -0,0 +1,69 @@
+import players
+import pool
+import teams
+
+team_name = "SuperSlug"
+team_gm_name = "Mitchell Elliott"
+team_gm_email = "email@gmail.com"
+team_gm_hometown = "City Name"
+team_gm_country = "USA"
+team_gm_pay_status = "Paid"
+team_gm_pay_method = "PayPal"
+team_gm_pay_amount = 10
+team_points = 200
+
+team_entry = []
+team_entry.append(team_name)
+team_entry.append(team_gm_name)
+team_entry.append(team_gm_email)
+team_entry.append(team_gm_hometown)
+team_entry.append(team_gm_country)
+team_entry.append(team_gm_pay_status)
+team_entry.append(team_gm_pay_method)
+team_entry.append(team_gm_pay_amount)
+print(team_entry)
+pool.create_pool_entry(team_entry)
+
+team_name = "Test"
+team_gm_name = "Team Name"
+team_gm_email = "email@gmail.com"
+team_gm_hometown = "City Name"
+team_gm_country = "USA"
+team_gm_pay_status = "Paid"
+team_gm_pay_method = "PayPal"
+team_gm_pay_amount = 10
+team_points = 300
+
+team_entry = []
+team_entry.append(team_name)
+team_entry.append(team_gm_name)
+team_entry.append(team_gm_email)
+team_entry.append(team_gm_hometown)
+team_entry.append(team_gm_country)
+team_entry.append(team_gm_pay_status)
+team_entry.append(team_gm_pay_method)
+team_entry.append(team_gm_pay_amount)
+print(team_entry)
+pool.create_pool_entry(team_entry)
+
+team_name = "Test2"
+team_gm_name = "Team Name"
+team_gm_email = "email@gmail.com"
+team_gm_hometown = "City Name"
+team_gm_country = "USA"
+team_gm_pay_status = "Paid"
+team_gm_pay_method = "PayPal"
+team_gm_pay_amount = 10
+team_points = 300
+
+team_entry = []
+team_entry.append(team_name)
+team_entry.append(team_gm_name)
+team_entry.append(team_gm_email)
+team_entry.append(team_gm_hometown)
+team_entry.append(team_gm_country)
+team_entry.append(team_gm_pay_status)
+team_entry.append(team_gm_pay_method)
+team_entry.append(team_gm_pay_amount)
+print(team_entry)
+pool.create_pool_entry(team_entry)
