4d39ea72906e7f192415b8fa0a554aeec0cdfdee
jcockroft64 <53958343+jcockroft64@users.noreply.github.com>
2020-08-12 22:10:06 -0700

    creating file fo demark team/player status

---
+++ include/constants.py
@@ -0,0 +1,11 @@
+'''
+    constants.py - This file defines cross module constants used in JHP.
+'''
+
+DNQ = 0    # team/player did not qualify for the playoffs
+R1_ELIM = 1 # team/player was eliminated from the 1st round of the playoffs
+R2_ELIM = 2 # team/player was eliminated from the 2nd round of the playoffs
+R3_ELIM = 3 # team/player was eliminated from the conference finals
+R4_ELIM = 4 # team/player was eliminated from the SC finals
+ACTIVE  = 5   # team/player is actively qualified for participating in the playoffs
+ACT_INJ = 6   # player is actively qualified to participate but is injured% 
