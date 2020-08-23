adcc24d652701dac8b718c20cf778e306a8a290b
Mitchell Elliott <melliott18@github.com>
2020-08-23 11:07:33 -0700

    Fixed null previous points error

--- lib/pool.py
+++ lib/pool.py
@@ -213,7 +213,10 @@ def update_points_change():
         cursor.execute(sql)
         prev_points = cursor.fetchone()
         #print(prev_points[0])
-        points_change = curr_points - int(prev_points[0])
+        try:
+            points_change = curr_points - int(prev_points[0])
+        except:
+            points_change = curr_points
         #print(points_change)
         #sql = "UPDATE pool_stats SET points_change = {chg} WHERE entry_id = '{id}'".format(chg=points_change, id=team[0])
         #cursor.execute(sql)
